a0d5e394bc7f254bd72cd40c938d460a7fb15f8c
Amanda Guerriero <ag.codingclass@gmail.com>
2020-08-29 13:36:07 -0700

    A few updates to save and print scores, but still not working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,32 +125,32 @@ function saveHighscore() {
        window.localStorage.setItem("highscores", JSON.stringify(highscores));
        // Redirect to High Scores Page
        window.location.href = "highscores.html";
-     }
-   }
+    }
+}
 
 // Submit Initials
 submitBtn.onclick = saveHighscore;
 
 // Show All Scores Funtion
 function printHighscores() {
-     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
      // Sort High Scores
-     highscores.sort(function (a, b) {
+    highscores.sort(function (a, b) {
        return b.score - a.score;
-     });
-     highscores.forEach(function (highscores) {
-       // create li tag for each high score
-       var liTag = document.createElement("li");
-       liTag.textContent = highscores.initials + " - " + highscores.score;
-       // display on page
-       var olEl = document.getElementById("high-score-list");
-       olEl.appendChild(liTag);
-     });
-   }
+    });
+    highscores.forEach(function (highscores) {
+        // Create each list item
+        var liTag = document.createElement("li");
+        liTag.textContent = highscores.initials + " - " + highscores.score;
+        // Display on page
+        var olEl = document.getElementById("high-score-list");
+        olEl.appendChild(liTag);
+    });
+}
    function clearHighscores() {
      window.localStorage.removeItem("highscores");
      window.location.reload();
-   }
+}
 
 
 // Clear Highscores - Nice to Have
