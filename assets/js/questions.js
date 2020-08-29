66da1d12432aba7a30c257604ad437b034b3eefa
Amanda Guerriero <ag.codingclass@gmail.com>
2020-08-28 19:56:32 -0700

    Printed options as buttons, created optionsClick function, cycled through questions

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+// Set Question & Answer Text
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include_______.",
+        options: ["strings", "booleans", "numbers", "alerts"],
+        answer: "alerts"
+    },
+    
+    {
+        question: "The condition in an if / else statement is enclosed within_______.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: ""
+    },
+
+    {
+        question: "Arrays in Javascript can be used to store_______.",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "Numbers"
+    },
+
+    {
+        question: "Sting values must be enclosed within ________ when being assigned to variables",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "1"
+    },
+
+    {
+        question: "A very useful tool used during development",
+        options: ["1", "2", "3", "4"],
+        answer: "1"
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,51 +6,60 @@ var currentQuestionIndex = 0;
 var startButton = document.getElementById("start-btn");
 var questionsEl = document.getElementById("questions");
 var optionsEl = document.getElementById("options");
+var goBackButton = document.getElementById("go-back-btn");
+var clearButton = document.getElementById("clear-btn");
+var optionsEl = document.getElementById("options");
 
 
 // Start Quiz Function
 var startQuiz = function () {
     
-// Hide Start Screen
-var startScreenEl = document.getElementById("start-screen");
-    startScreenEl.setAttribute("class", "hide");
+    // Hide Start Screen
+    var startScreenEl = document.getElementById("start-screen");
+        startScreenEl.setAttribute("class", "hide");
 
-// Display Questions
-showQuestions();
+    // Display Questions
+    showQuestions(currentQuestionIndex);
 }
 
 // Show Questions Function
 var showQuestions = function() {
     var currentQuestion = questions[currentQuestionIndex];
-    
-    // var optionsEl = document.getElementById("options")
-    // optionsEl.innerHTML = "";
 
-    questions.forEach((titleEl, optionsEl) => {
         var titleEl = document.getElementById("q-title");
-        titleEl.textContent = currentQuestion.question;
-        var optionsEl = document.getElementById("options")
-        optionsEl.innerHTML = currentQuestion.options;
-    });
+            titleEl.textContent = currentQuestion.question;
+        
+            // Display Questions
+            currentQuestion.options.forEach((option, currentQuestionIndex) => {
+                var optionsButtons = document.createElement ("button")
+                optionsButtons.setAttribute ("value", option)
+                optionsButtons.textContent = currentQuestionIndex + 1 + "." + option;
+                optionsButtons.onclick = optionsClick;
+                optionsEl.appendChild(optionsButtons);
+            });
+}
 
-    for(var i = 0; i < questions.length; i++) {
-        var answer = (questions[i].question);
-        if (answer === questions[i].answer) {
+var optionsClick = function () {
+    console.log ("I was clicked")
+    // for(var i = 0; i < questions.length; i++) {
+        var answer = currentQuestionIndex.answer;
+        if (answer === currentQuestionIndex.answer) {
             score++;
             console.log("Correct");
         }    
-        else {
+        else (timer = 16 || timer < 16); {
             timer -= 15;
             console.log("Wrong");
         }
+    currentQuestionIndex ++;
+    showQuestions ();
     }
-}
-
+//}
 
 
 // Countdown Timer
 var countdownTimer = setInterval(function(){
-    if(timer <= 1){
+    if(timer <= 0){
     clearInterval(countdownTimer);
     document.getElementById("timer").innerHTML = "No Time Left";
     console.log("No Time Left");
@@ -62,44 +71,11 @@ var countdownTimer = setInterval(function(){
   document.getElementById("timer").innerHTML = "<h1>Timer </h1>" + timer;
 }, 1000);
 
-// Set Question & Answer Text
-const questions = [
-    {
-        question: "Commonly used data types DO NOT include_______.",
-        options: ["strings", "booleans", "numbers", "alerts"],
-        answer: "alerts"
-    },
-    
-    {
-        question: "The condition in an if / else statement is enclosed within_______.",
-        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: ""
-    },
-
-    {
-        question: "Arrays in Javascript can be used to store_______.",
-        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "Numbers"
-    },
-
-    {
-        question: "Sting values must be enclosed within ________ when being assigned to variables",
-        options: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "1"
-    },
-
-    {
-        question: "A very useful tool used during development",
-        options: ["1", "2", "3", "4"],
-        answer: "1"
-    }
-]
 
 // document.getElementById("questions").textContent = questions;
 
 
 // Show User's Score
-console.log("You Scored" + score + " points.");
 
 // Save Score and initials to local Storage
 
@@ -108,6 +84,7 @@ console.log("You Scored" + score + " points.");
 
 
 // Clear Highscores - Nice to Have
+goBackButton.addEventListener("click", startQuiz);
 
 // Go Back - Nice to Have
 
